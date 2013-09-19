132b80f1bc777baffab47c7756458b7a7d086734
Christopher Hunt <mail@thisisthechris.co.uk>
2013-09-19 19:46:05 +0100

    Adds Check In Holders View

--- shipmates/views.py
+++ shipmates/views.py
@@ -5,18 +5,49 @@ from django.contrib.auth.models import User
 from django.conf import settings
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 
+
 from django import forms
 
 from django.contrib.auth.decorators import login_required
 
 from models import *
 
-def list_start_times(request):
-	types_list = TicketType.objects.all()
+import collections
 
-	return render_to_response('list_start_times.html', {
+def make_player_form(quantity, first_player):
+    fields = {}
+    count = 1
+
+    while count <= quantity:
+        field_name = ''
+
+        if count < 10:
+            field_name = '0'+str(count)
+        else:
+            field_name = str(count)
+
+        if count == 1:
+            fields[field_name] = forms.CharField(
+                initial=first_player,
+                error_messages={'required': 'Please enter a name'}
+            )
+        else:
+            fields[field_name] = forms.CharField(
+                error_messages={'required': 'Please enter a name'}
+            )
+        count += 1
+
+        fields = collections.OrderedDict(sorted(fields.items()))
+
+    return type('player_form', (forms.BaseForm,), { 'base_fields': fields })
+
+def list_start_times(request):
+    types_list = TicketType.objects.all()
+
+    return render_to_response('list_start_times.html', {
         'types_list': types_list
     }, context_instance=RequestContext(request))
+
 
 def list_ticket_holders(request, type_id):
     the_type = get_object_or_404(TicketType, pk=type_id)
@@ -26,3 +57,31 @@ def list_ticket_holders(request, type_id):
         'holder_list': holder_list,
         'start_time': the_type.name
     }, context_instance=RequestContext(request))
+
+def check_in_holders(request, holder_id):
+    the_holder = get_object_or_404(TicketHolder, pk=holder_id)
+
+    the_form = make_player_form(the_holder.quantity, the_holder.name)
+
+    success = False
+
+    if request.method == "POST":
+        the_form = the_form(request.POST)
+        if the_form.is_valid():
+            for key, value in the_form.cleaned_data.iteritems():
+                the_player = Player()
+                the_player.name = value
+                the_player.ticketholder = the_holder
+                the_player.save()
+
+            the_holder.checked_in = True
+            the_holder.save()
+
+            return redirect('/shipmates/time/'+str(the_holder.tickettype.id)+'/')       
+    
+
+    return render_to_response('check_in_holders.html', {
+        'the_holder': the_holder,
+        'the_form': the_form
+    }, context_instance=RequestContext(request))
+
--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
      (r'^shipmates/time/(?P<type_id>\d+)/$', 'shipmates.views.list_ticket_holders'),
+     (r'^shipmates/holder/(?P<holder_id>\d+)/$', 'shipmates.views.check_in_holders'),
      (r'^shipmates/', 'shipmates.views.list_start_times'),
 )
