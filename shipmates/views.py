2e40270f1254928e14d2238eead30bfbe3abea7a
Christopher Hunt <mail@thisisthechris.co.uk>
2013-09-19 17:55:49 +0100

    Adds List Start Time View

--- shipmates/views.py
+++ shipmates/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Context, loader, RequestContext
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.conf import settings
+from django.shortcuts import render_to_response, get_object_or_404, redirect
+
+from django import forms
+
+from django.contrib.auth.decorators import login_required
+
+from models import *
+
+def list_start_times(request):
+	types_list = TicketType.objects.all()
+
+	return render_to_response('list_start_times.html', {
+        'types_list': types_list
+    }, context_instance=RequestContext(request))
+
--- urls.py
+++ urls.py
@@ -5,4 +5,5 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
      url(r'^admin/', include(admin.site.urls)),
+     url(r'^shipmates/', 'shipmates.views.list_start_times')
 )
