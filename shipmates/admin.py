19c90082f1f7a11b21a15dbe96f4dc8987683a93
Christopher Hunt <mail@thisisthechris.co.uk>
2013-09-27 14:36:46 +0100

    Adds excel exports

--- shipmates/admin.py
+++ shipmates/admin.py
@@ -1,8 +1,14 @@
 from models import *
 
 from django.contrib import admin
+from shipmates.actions import export_as_xls
 
 admin.site.register(TicketType)
 admin.site.register(TicketHolder)
 admin.site.register(Player)
 admin.site.register(Crew)
+
+class MyAdmin(admin.ModelAdmin):
+    actions = [export_as_xls]
+
+admin.site.add_action(export_as_xls)
